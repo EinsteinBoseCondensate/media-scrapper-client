26876a989c6ce3a18ba087c217d541b031ad09a1
EinsteinBoseCondensate <antoniom11190@gmail.com>
2023-11-20 18:23:40 +0100

    missing injectable decorator

--- src/app/shared/interceptors/auth-extension-http-interceptor.ts
+++ src/app/shared/interceptors/auth-extension-http-interceptor.ts
@@ -2,7 +2,9 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/c
 import { Observable } from "rxjs";
 import { Auth0JwtService } from "../services/auth0-jwt-service";
 import { environment as env } from '../../../environments/environment';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class Auth0AuthnExtensionHttpInterceptor implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         if(this.isUrlBindedToHeaderAppendNeed(req.url))
