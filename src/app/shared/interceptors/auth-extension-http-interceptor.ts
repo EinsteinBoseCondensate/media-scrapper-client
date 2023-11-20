d787c01495f80132ded59ab5497e76471dd52f7e
EinsteinBoseCondensate <antoniom11190@gmail.com>
2023-11-20 19:01:48 +0100

    changed headers modification at http interceptor

--- src/app/shared/interceptors/auth-extension-http-interceptor.ts
+++ src/app/shared/interceptors/auth-extension-http-interceptor.ts
@@ -5,18 +5,22 @@ import { environment as env } from '../../../environments/environment';
 import { Injectable } from "@angular/core";
 
 @Injectable()
-export class Auth0AuthnExtensionHttpInterceptor implements HttpInterceptor{
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-        if(this.isUrlBindedToHeaderAppendNeed(req.url))
-            req.headers.set('Authorization', `Bearer ${Auth0JwtService.rawToken}`);
+export class Auth0AuthnExtensionHttpInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (this.isUrlBindedToHeaderAppendNeed(req.url)) {
+            const clonedRequest = req.clone({
+                setHeaders:{
+                    'Authorization': `Bearer ${Auth0JwtService.rawToken}`
+                }
+            });
+            return next.handle(clonedRequest);
+        }
 
         return next.handle(req);
     }
-    isUrlBindedToHeaderAppendNeed(url: string): boolean{
-        console.log(url);
-        console.log(env.authExtensionHttpInterceptor.allowedList);
+    isUrlBindedToHeaderAppendNeed(url: string): boolean {
         return !!env.authExtensionHttpInterceptor.allowedList.filter(address => {
-            if(address.includes('/*')) {
+            if (address.includes('/*')) {
                 return url.startsWith(address.replace('/*', ''))
             }
             return address === url;
